60292abe26402df76335e6282b6582fcad068381
Riccardo <riky24.mo@gmail.com>
2016-06-15 10:39:52 +0200

    controllo login

--- src/it/uniroma3/controller/PrenotazioneController.java
+++ src/it/uniroma3/controller/PrenotazioneController.java
@@ -33,50 +33,50 @@ public class PrenotazioneController extends HttpServlet  {
 		HttpSession s = request.getSession();
 		String nextpage;
 
-		//		Utente utente = (Utente)s.getAttribute("utente");
-		//		if(utente==null || utente.getRuolo().equals("user")){
-		//			if (utente.getRuolo().equals("user")) {
-		//				request.setAttribute("loginError", "Effettua il login come amministratore");
-		//			} else
-		//				request.setAttribute("loginError", "Effettua il login");
-		//			nextpage = "/effettuaLogin.jsp";
-		//		}else{
+		Utente utente = (Utente)s.getAttribute("utente");
+		if(utente==null || utente.getRuolo().equals("user")){
+			if (utente.getRuolo().equals("user")) {
+				request.setAttribute("loginError", "Effettua il login come amministratore");
+			} else
+				request.setAttribute("loginError", "Effettua il login");
+			nextpage = "/effettuaLogin.jsp";
+		}else{
 
-		nextpage = "/creaPrenotazione.jsp";
+			nextpage = "/creaPrenotazione.jsp";
 
-		Facade f = new Facade();
-		f.istanziaEntityManager();
-		String str = f.generaSelectTipologia();
-		request.setAttribute("string", str);
+			Facade f = new Facade();
+			f.istanziaEntityManager();
+			String str = f.generaSelectTipologia();
+			request.setAttribute("string", str);
 
-		String codFis = request.getParameter("codFis");
-		String idTipologiaEsame = request.getParameter("tipologia");
+			String codFis = request.getParameter("codFis");
+			String idTipologiaEsame = request.getParameter("tipologia");
 
-		if(idTipologiaEsame!=null){
-			boolean errore = false;
-			if(codFis.equals("")){
-				request.setAttribute("pazienteError", "Selezionare un paziente");
-				errore = true;
-			}
-			if(!errore){
-				Utente u = f.getByCodFiscale(codFis);
-				if(!(idTipologiaEsame.equals(""))){
-					Long tipId = Long.parseLong(idTipologiaEsame);
-					TipologiaEsame t = f.getTipologiaByID(tipId);
+			if(idTipologiaEsame!=null){
+				boolean errore = false;
+				if(codFis.equals("")){
+					request.setAttribute("pazienteError", "Selezionare un paziente");
+					errore = true;
+				}
+				if(!errore){
+					Utente u = f.getByCodFiscale(codFis);
+					if(!(idTipologiaEsame.equals(""))){
+						Long tipId = Long.parseLong(idTipologiaEsame);
+						TipologiaEsame t = f.getTipologiaByID(tipId);
 
-					Prenotazione p = new Prenotazione();
-					p.setUtente(u);
-					p.setTipologiaEsame(t);
+						Prenotazione p = new Prenotazione();
+						p.setUtente(u);
+						p.setTipologiaEsame(t);
 
-					f.inserisciPrenotazione(p);
+						f.inserisciPrenotazione(p);
 
-					nextpage = "/confermaPrenotazione.jsp";
+						nextpage = "/confermaPrenotazione.jsp";
+					}
 				}
 			}
+
+
 		}
-
-
-		//		}
 		ServletContext servletContext = getServletContext();
 		RequestDispatcher rd = servletContext.getRequestDispatcher(nextpage);
 		rd.forward(request, response);
